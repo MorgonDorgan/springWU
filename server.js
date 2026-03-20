6a705bd6c34f116295e7ba7b2c4411a52e5734c2
MorgonDorgan <isac.eriksson3@elev.ga.ntig.se>
2026-03-20 14:20:55 +0100

    note to self remove notes

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/searchDB")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+  const ItemSchema = new mongoose.Schema({
+    name: String
+  });
+  
+  const Item = mongoose.model("Item", ItemSchema);
 
 const data = [
   "HTML",
@@ -22,6 +33,16 @@ app.get("/search", (req, res) => {
 
   res.json(results);
 
+});
+
+app.get("/add", async (req, res) => {
+  await Item.create({ name: "HTML" });
+  await Item.create({ name: "CSS" });
+  await Item.create({ name: "JavaScript" });
+  await Item.create({ name: "Node.js" });
+  await Item.create({ name: "Express" });
+
+  res.send("Data added");
 });
 
 app.listen(3000, () => {
